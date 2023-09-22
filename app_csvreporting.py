ecbf3d6f40f9618958bfd8a54b5d5aeb86e7ab53
lr2t9iz <lr2t9iz@me.local>
2023-09-22 00:58:28 +0000

    init

---
+++ app_csvreporting.py
@@ -0,0 +1,9 @@
+# Written by lr2t9iz (2023.09.25)
+# Script to generate and csv report and attaches it in an email
+
+# Stepts
+#  1. Fetch Wazuh Security Events (data)
+#  2. Process data and generate a csv report
+#  3. Send an email with csv report attached
+
+if __name__=="__main__":
